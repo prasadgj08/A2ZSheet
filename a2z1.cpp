b2257f596d6a60a088325f95fe3f0bc5e5dc31ac
Prasad <prasadgjoshi2008@gmail.com>
2026-07-17 09:14:07 +0530

    .

--- a2z1.cpp
+++ a2z1.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 int main() {
+    int n=5;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 
-    cout<<"hello";
+
+
     return 0;
 }
