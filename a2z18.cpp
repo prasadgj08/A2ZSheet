58846c370d2ac580e3756fd830d2a14e263e0325
Prasad <prasadgjoshi2008@gmail.com>
2026-07-17 21:36:11 +0530

    Pattern!

---
+++ a2z18.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    char n='D';
+
+    for(char i=n; i>='A'; i--) {
+        for(char j=i; j<=n; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+} //fLaWlEsS
